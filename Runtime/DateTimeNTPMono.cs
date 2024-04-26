bc03e64676dc3314ef00f30230dbbf5cfc5e53b3
Éloi Strée <eloistree@gmail.com>
2024-04-26 22:40:06 +0200

    Save: 04/26/2024 10:40 PM

--- Runtime/DateTimeNTPMono.cs
+++ Runtime/DateTimeNTPMono.cs
@@ -4,16 +4,28 @@ using UnityEngine;
 public class DateTimeNTPMono: MonoBehaviour
 {
     public string m_ntpServer= "time.windows.com";
-    public string m_currentTimeOnPC;
+    public string m_currentTimeOnPc;
     public string m_currentTimeOnNtp;
-    public DateTime m_currentTimeOnPCDate;
+    public long m_currentTimeOnPcTick;
+    public long m_currentTimeOnNtpTick;
+    public long m_differencePcNtpTick;
+    public double m_differencePcNtpMilliseconds;
+    public DateTime m_currentTimeOnPcDate;
     public DateTime m_currentTimeOnNtpDate;
 
     private void Awake()
-    {
-        m_currentTimeOnPCDate = DateTime.UtcNow.ToUniversalTime();
+    { Refresh(); }
+
+    [ContextMenu("Refresh")]
+    public void Refresh() { 
+        m_currentTimeOnPcDate = DateTime.UtcNow.ToUniversalTime();
         m_currentTimeOnNtpDate = DateTimeNTP.GetNetworkTime().ToUniversalTime();
-        m_currentTimeOnPC = m_currentTimeOnPCDate.ToString();
+        m_currentTimeOnPc = m_currentTimeOnPcDate.ToString();
         m_currentTimeOnNtp = m_currentTimeOnNtpDate.ToString();
+        m_currentTimeOnPcTick =m_currentTimeOnPcDate.Ticks ;
+        m_currentTimeOnNtpTick= m_currentTimeOnNtpDate.Ticks;
+        m_differencePcNtpTick = m_currentTimeOnNtpTick - m_currentTimeOnPcTick;
+        m_differencePcNtpMilliseconds = m_differencePcNtpTick / TimeSpan.TicksPerMillisecond;
+
     }
 }
