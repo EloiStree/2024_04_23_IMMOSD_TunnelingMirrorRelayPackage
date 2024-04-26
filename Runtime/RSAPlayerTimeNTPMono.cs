f9340d259ca5b9343f1d852f232e07dcc6581e9e
Éloi Strée <eloistree@gmail.com>
2024-04-26 21:56:58 +0200

    Save: 04/26/2024 9:56 PM

--- Runtime/RSAPlayerTimeNTPMono.cs
+++ Runtime/RSAPlayerTimeNTPMono.cs
@@ -1,20 +1,76 @@
 using Mirror;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class RSAPlayerTimeNTPMono : NetworkBehaviour
 {
-    public MirrorPlayerMono_RSAHandshake m_handshake;
+    //https://youtu.be/BAo5C2qbLq8?t=238-
 
-    void Start()
+    public NTPLongKey m_timeDifference;
+
+
+
+    [SyncVar]
+    public double m_pingSentReceived;
+
+    [SyncVar]
+    public long m_differenceInTick;
+
+
+
+    [ContextMenu("Refresh Timing")]
+    public void RefreshTiming()
     {
-        
+        NTPLongKey timePack = new NTPLongKey();
+        timePack.m_t0_dateTimeClientSent = TicksLocalMachineUTC();
+        CmdPushTimeStart(timePack);
     }
 
-    // Update is called once per frame
-    void Update()
+    private static long TicksLocalMachineUTC()
     {
-        
+        return DateTime.UtcNow.Ticks;
+    }
+
+    [Command]
+    public void CmdPushTimeStart(NTPLongKey time)
+    {
+       time. m_t1_dateTimeGenerateServerStart = TicksLocalMachineUTC();
+       time. m_t2_dateTimeGenerateServerSent = TicksLocalMachineUTC();
+        RpcNotifyClientThatTimeIsReceived(time);
+    }
+    [ClientRpc]
+    public void RpcNotifyClientThatTimeIsReceived(NTPLongKey time)
+    {
+        time.m_t3_dateTimeClientReceived = TicksLocalMachineUTC();
+        CmdPushTimeReceived(time);
+        //OnClient
+        ComputeDifferenceInTick(time);
+    }
+    [Command]
+    public void CmdPushTimeReceived(NTPLongKey time)
+    {
+        //OnServer
+        ComputeDifferenceInTick(time);
+    }
+
+    
+
+    private void ComputeDifferenceInTick(NTPLongKey time)
+    {
+        m_timeDifference = time;
+        m_pingSentReceived = time.m_t3_dateTimeClientReceived - time.m_t0_dateTimeClientSent;
+        m_differenceInTick =  
+            (time.m_t3_dateTimeClientReceived - time.m_t0_dateTimeClientSent)
+            - (time.m_t2_dateTimeGenerateServerSent - time.m_t1_dateTimeGenerateServerStart);
+    }
+
+    [System.Serializable]
+    public struct NTPLongKey {
+        public long m_t0_dateTimeClientSent;
+        public long m_t1_dateTimeGenerateServerStart;
+        public long m_t2_dateTimeGenerateServerSent;
+        public long m_t3_dateTimeClientReceived;
     }
 }
