42cbe377817405e81dfddf085d1729b8ae04f725
Éloi Strée <eloistree@gmail.com>
2024-04-27 18:02:56 +0200

    Save: 04/27/2024 6:02 PM

--- Runtime/#SleepyTest/Sleep_SoundTest.cs
+++ Runtime/#SleepyTest/Sleep_SoundTest.cs
@@ -14,26 +14,33 @@ public class Sleep_SoundTest : MonoBehaviour
     public UnityEvent<Color> m_onTickColor;
 
     public int m_deciSeconds=50;
-    public int m_deciseconds;
+    public int m_receivedDeciSecondsTotale;
     public int m_previousDeciSeconds;
     public int m_currentDeciSeconds;
+    public int m_colorIndex;
+
+    public Color[] m_colorArray= new Color[] { Color.red * 0.2f, Color.green * 0.2f, Color.blue * 0.2f};
+
+    public bool m_useMainCameraDebug=true;
+
     void Update()
     {
-        m_deciseconds =(int)( (m_useNtp ? m_ntp.GetAdjustedTime().Ticks : DateTime.UtcNow.Ticks) / (TimeSpan.TicksPerSecond/10));
-         m_currentDeciSeconds = (int) ((m_deciseconds) % m_deciSeconds);
+        m_receivedDeciSecondsTotale =(int)( (m_useNtp ? m_ntp.GetAdjustedTime().Ticks : DateTime.UtcNow.Ticks) / (TimeSpan.TicksPerSecond/10));
+        m_currentDeciSeconds = (int) ((m_receivedDeciSecondsTotale) % m_deciSeconds);
         if (m_currentDeciSeconds != m_previousDeciSeconds) {
             m_previousDeciSeconds = m_currentDeciSeconds;
-            if(m_currentDeciSeconds == 0)
-                Tick();
+            if(m_currentDeciSeconds==0)
+                Tick(m_receivedDeciSecondsTotale);
         }
     }
 
-    private void Tick()
+    private void Tick(int value)
     {
+        m_colorIndex = (Math.Abs(value) / m_deciSeconds) % m_colorArray.Length;
         m_onTick.Invoke();
-        Color c  = new Color(UnityEngine.Random.value, UnityEngine.Random.value, UnityEngine.Random.value);
+        Color c = m_colorArray[m_colorIndex];
         m_onTickColor.Invoke(c);
-        if (Camera.main)
+        if (m_useMainCameraDebug && Camera.main)
             Camera.main.backgroundColor = c;
     }
 }
